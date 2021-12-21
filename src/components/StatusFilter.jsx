3e1cdd6393101c8529eb3a376bd15c2cf7fd8f17
AofPatiphan <patiphan.ma@hotmail.com>
2021-12-21 14:11:50 +0700

    save

--- src/components/StatusFilter.jsx
+++ src/components/StatusFilter.jsx
@@ -1,47 +1,55 @@
 function StatusFilter(props) {
-    const handleChange = e => {
-      const value = e.target.value;
-      props.setSearchStatus(!value ? value : value === 'true');
+    const handleChange = (e) => {
+        const value = e.target.value;
+        props.setSearchStatus(!value ? value : value === 'true');
     };
     return (
-      <div className="btn-group ms-3">
-        <input
-          type="radio"
-          className="btn-check"
-          id="all"
-          name="status"
-          defaultChecked
-          onChange={handleChange}
-          value=""
-        />
-        <label className="btn btn-outline-secondary rounded-0" htmlFor="all">
-          <i className="bi bi-list-check" />
-        </label>
-        <input
-          type="radio"
-          className="btn-check"
-          id="done"
-          name="status"
-          value="true"
-          onChange={handleChange}
-        />
-        <label className="btn btn-outline-secondary rounded-0" htmlFor="done">
-          <i className="bi bi-clipboard-check" />
-        </label>
-        <input
-          type="radio"
-          className="btn-check"
-          id="doing"
-          name="status"
-          value="false"
-          onChange={handleChange}
-        />
-        <label className="btn btn-outline-secondary rounded-0" htmlFor="doing">
-          <i className="bi bi-clipboard" />
-        </label>
-      </div>
+        <div className="btn-group ms-3">
+            <input
+                type="radio"
+                className="btn-check"
+                id="all"
+                name="status"
+                defaultChecked
+                onChange={handleChange}
+                value=""
+            />
+            <label
+                className="btn btn-outline-secondary rounded-0"
+                htmlFor="all"
+            >
+                <i className="bi bi-list-check" />
+            </label>
+            <input
+                type="radio"
+                className="btn-check"
+                id="done"
+                name="status"
+                value="true"
+                onChange={handleChange}
+            />
+            <label
+                className="btn btn-outline-secondary rounded-0"
+                htmlFor="done"
+            >
+                <i className="bi bi-clipboard-check" />
+            </label>
+            <input
+                type="radio"
+                className="btn-check"
+                id="doing"
+                name="status"
+                value="false"
+                onChange={handleChange}
+            />
+            <label
+                className="btn btn-outline-secondary rounded-0"
+                htmlFor="doing"
+            >
+                <i className="bi bi-clipboard" />
+            </label>
+        </div>
     );
-  }
-  
-  export default StatusFilter;
-  
+}
+
+export default StatusFilter;
